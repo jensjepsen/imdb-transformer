70faceae230d0219c2b456620c54db588f77f980
bandit <bandit@10endebandit.aau.dk>
2018-10-25 13:22:48 +0200

    update

--- train.py
+++ train.py
@@ -32,17 +32,18 @@ def val(model,test,vocab,device):
             total += b.label.size(0)
         print "{}%, {}/{}".format(correct / total,correct,total)
 
-def train(max_length,hidden_size,
+def train(max_length,model_size,
             epochs,learning_rate,
-            device,num_heads,
-            dropout,train_word_embeddings):
+            device,num_heads,num_blocks,
+            dropout,train_word_embeddings,
+            batch_size):
     """
         Trains the classifier on the IMDB sentiment dataset
     """
-    train, test, vectors, vocab = get_imdb(hidden_size,max_length=max_length)
+    train, test, vectors, vocab = get_imdb(batch_size,max_length=max_length)
 
     model = Net(
-                hidden_size=hidden_size,embeddings=vectors,
+                model_size=model_size,embeddings=vectors,
                 max_length=max_length,num_heads=num_heads,
                 num_blocks=num_blocks, dropout=dropout,
                 train_word_embeddings=train_word_embeddings,
@@ -70,29 +71,30 @@ if __name__ == "__main__":
     import argparse
     ap = argparse.ArgumentParser(description="Train a Transformer network for sentiment analysis")
     
-    ap.add_argument("--max_length",default=500,dest="max_length",help="Maximum sequence length, \
+    ap.add_argument("--max_length",default=500,type=int,help="Maximum sequence length, \
                                                                     sequences longer than this are truncated")
     
-    ap.add_argument("--hidden_size",default=128,dest="hidden_size",help="Hidden size for all \
+    ap.add_argument("--model_size",default=128,type=int,help="Hidden size for all \
                                                                     hidden layers of the model")
     
-    ap.add_argument("--epochs",default=1000,dest="epochs",help="Number of epochs to train for")
+    ap.add_argument("--epochs",default=1000,type=int,help="Number of epochs to train for")
 
-    ap.add_argument("--learning_rate",default=0.001,dest="learning_rate",help="Learning rate for optimizer")
+    ap.add_argument("--learning_rate",default=0.001,type=float,dest="learning_rate",help="Learning rate for optimizer")
     
     ap.add_argument("--device",default="cuda:0",dest="device",help="Device to use for training \
                                                                     and evaluation e.g. (cpu, cuda:0)")
-    ap.add_argument("--num_heads",default=4,dest="num_heads",help="Number of attention heads in \
+    ap.add_argument("--num_heads",default=4,type=int,dest="num_heads",help="Number of attention heads in \
                                                                     the Transformer network")
     
-    ap.add_argument("--num_blocks",default=1,dest="num_blocks",help="Number of blocks in the Transformer network")
+    ap.add_argument("--num_blocks",default=1,type=int,dest="num_blocks",help="Number of blocks in the Transformer network")
     
-    ap.add_argument("--dropout",default=1,dest="dropout",help="Dropout (not keep_prob, but probability of ZEROING \
+    ap.add_argument("--dropout",default=0.1,type=float,dest="dropout",help="Dropout (not keep_prob, but probability of ZEROING \
                                                                     during training, i.e. keep_prob = 1 - dropout)")
 
-    ap.add_argument("--train_word_embeddings",default=True,dest="train_word_embeddings",help="Train GloVE word embeddings")
-
-    args = ap.parse_args()
+    ap.add_argument("--train_word_embeddings",type=bool,default=True,dest="train_word_embeddings",help="Train GloVE word embeddings")
+    
+    ap.add_argument("--batch_size",type=int,default=128,help="Batch size")
+    args = vars(ap.parse_args())
 
     train(**args)
 
